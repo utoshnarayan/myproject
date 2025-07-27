290c768d6820970ef80769ef0db9f1a1dc24e965
UTOSH NARAYAN <utoshnarayan@gmail.com>
2025-07-27 15:47:50 +0530

    Update UpdateEmployee.jsx

--- my-client/src/components/UpdateEmployee.jsx
+++ my-client/src/components/UpdateEmployee.jsx
@@ -13,7 +13,7 @@ export function UpdateEmployee(){
         e.preventDefault();
         try
         {
-            const response = await axios.put(`http://localhost:3001/api/employees/${id}`, { empNo: Number(empNo), empName, empSal: Number(empSal) });
+            const response = await axios.put(`https://employee-management-utosh.onrender.com/api/employees/${id}`, { empNo: Number(empNo), empName, empSal: Number(empSal) });
             alert(response.data.message);
         }
         catch(err)
