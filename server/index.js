3c0506520d8e46b5faafd444ad800001c30a66d3
UTOSH NARAYAN <utoshnarayan@gmail.com>
2025-07-27 15:55:11 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express= require('express');
 const mongoose= require('mongoose');
-const port= process.env.PORT || 3001;
+const port= 'https://utosh-employee-management.onrender.com';
 const app= express();
 const cors=require('cors')
 
